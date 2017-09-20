bc3cd2611715c960883895695c0ae8ba6ed09e82
Ryan Haney <ryan@appology.net>
2017-09-20 03:19:50 -0700

    Now asking for license info, cleanup

--- bin/nearleyt.js
+++ bin/nearleyt.js
@@ -29,54 +29,56 @@ if (command === 'init') {
 
       const default_package_name = path.basename(process.cwd())
       const default_package_description = 'A grammar and stream-friendly parser.';
+      const default_package_license = 'MIT';
       rl.question(`package name: (${default_package_name}) `, package_name => {
         rl.question('package version: (0.0.1) ', package_version => {
           rl.question(`package description: (${default_package_description}) `, package_description => {
-            rl.question('github username: ', github_username => {
-              rl.question('github repo: ', github_repo => {
-                rl.close()
-                info.package_name = package_name.trim() || default_package_name
-                info.package_version = package_version.trim() || '0.0.1'
-                info.package_description = package_description.trim() || default_package_description
-                info.github_username = github_username.trim() || '{GITHUB_USERNAME}'
-                info.github_repo = github_repo.trim() || '{GITHUB_REPO}'
+            rl.question(`package license: (${default_package_license}) `, package_license => {
+              rl.question('github username: ', github_username => {
+                rl.question('github repo: ', github_repo => {
+                  rl.close()
+                  info.package_name = package_name.trim() || default_package_name
+                  info.package_version = package_version.trim() || '0.0.1'
+                  info.package_description = package_description.trim() || default_package_description
+                  info.package_license = package_license.trim() || default_package_license
+                  info.github_username = github_username.trim() || '{GITHUB_USERNAME}'
+                  info.github_repo = github_repo.trim() || '{GITHUB_REPO}'
 
-                // copy files
-                spinner.start('Copying files...')
-                if (shell.exec(`cp -r ${path.join(__dirname, '../template/')} . >> nearleyt.log 2>&1`).code != 0) {
-                  spinner.fail('Unable to copy template. Check nearleyt.log for details.')
-                  shell.exit(1)
-                }
-                spinner.succeed('Files copied.')
+                  // copy files
+                  spinner.start('Copying files...')
+                  if (shell.exec(`cp -r ${path.join(__dirname, '../template/')} . >> nearleyt.log 2>&1`).code != 0) {
+                    spinner.fail('Unable to copy template. Check nearleyt.log for details.')
+                    shell.exit(1)
+                  }
+                  spinner.succeed('Files copied.')
 
-                // patch files (e.g. package.json with project_name, project_description, etc.)
-                spinner.start('Patching files...')
-                var files = ['package.json', 'README.md']
-                shell.sed('-i', 'package_name', info.package_name, files)
-                shell.sed('-i', 'package_version', info.package_version, files)
-                shell.sed('-i', 'package_description', info.package_description, files)
-                shell.sed('-i', 'github_username', info.github_username, files)
-                shell.sed('-i', 'github_repo', info.github_repo, files)
-                spinner.succeed('Files patched.')
+                  // patch files (e.g. package.json with project_name, project_description, etc.)
+                  spinner.start('Patching files...')
+                  var files = ['package.json', 'README.md']
+                  Object.keys(info).forEach(i => {
+                    shell.sed('-i', i, info[i], files)
+                  })
+                  spinner.succeed('Files patched.')
 
-                // npm i
-                spinner.start('Installing dependencies...')
-                if (shell.exec('npm i >> nearleyt.log 2>&1').code != 0) {
-                  spinner.fail('Dependency installation failed. Check nearleyt.log for details.')
-                  shell.exit(1)
-                }
-                spinner.succeed('Dependencies installed.')
+                  // npm i
+                  spinner.start('Installing dependencies...')
+                  if (shell.exec('npm i >> nearleyt.log 2>&1').code != 0) {
+                    spinner.fail('Dependency installation failed. Check nearleyt.log for details.')
+                    shell.exit(1)
+                  }
+                  spinner.succeed('Dependencies installed.')
 
-                // npm test
-                spinner.text = 'Running tests...'
-                if (shell.exec('npm test >> nearleyt.log 2>&1').code != 0) {
-                  spinner.fail('Unable to run tests, or some tests failed. Check nearleyt.log for details.')
-                  shell.exit(1)
-                }
-                spinner.succeed('Tests passed!')
+                  // npm test
+                  spinner.text = 'Running tests...'
+                  if (shell.exec('npm test >> nearleyt.log 2>&1').code != 0) {
+                    spinner.fail('Unable to run tests, or some tests failed. Check nearleyt.log for details.')
+                    shell.exit(1)
+                  }
+                  spinner.succeed('Tests passed!')
 
-                // display completed message
-                shell.echo('Template generation complete!')
+                  // display completed message
+                  shell.echo('Template generation complete!')
+                })
               })
             })
           })
